cf49a54618d7d8dfb59f2bb3b1721d0bfdfaae39
Sunu Aziz Rahayu <me@kangsunu.com>
2022-07-27 08:15:41 +0700

    add source code

---
+++ src/@dist/js/script.js
@@ -0,0 +1,16 @@
+/*!
+ * PLEASE DON'T REMOVE THIS CREDIT
+ * -----------------------------------------------
+ * Original Themes Information: 
+ * Name          : Mediumish - A Medium style template by WowThemes.net
+ * URL           : https://www.wowthemes.net/mediumish-free-bootstrap-4-0-html-template-medium-styled/
+ * 
+ * Convert into Blogger Version:
+ * Name          : Mediumish Blogger Template
+ * Version       : 0.3.2
+ * Last Update   : 7 June 2020
+ * Init by       : Sunu Aziz Rahayu
+ * Theme URL     : https://github.com/sunuazizrahayu/free-mediumish-blogger-template
+ * Theme Preview : https://free-mediumish.blogspot.com/
+ * ----------------------------------------------- */
+"use strict";
